a63c5c5daeeed1b5683a3af142b7a2bdf82a1280
Twirl1511 <64476851+Twirl1511@users.noreply.github.com>
2021-01-17 00:43:54 +0300

    разбираюсь с управлением

--- IceGuard/Assets/Scripts/PlayerController.cs
+++ IceGuard/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public AnimationCurve Easing;
+    [SerializeField] float TimeToReachNextTile = 3;
+    private Vector3 _startPosition;
+    private Vector3 _endPosition;
+    private float _time;
     void Start()
     {
-        
+        _startPosition = transform.position;
+        _endPosition = _startPosition + new Vector3(-1.1f, 0f, 0f);
+    }
+    private void FixedUpdate()
+    {
+        Debug.Log(Input.GetAxis("Mouse X")); 
+        if(Input.GetAxis("Mouse X") > 0.5f)
+        {
+            transform.rotation = Quaternion.Euler(0f, 90f, 0f);
+        }
+        //if (Input.GetAxis("Mouse X") < -0.5f)
+        //{
+        //    transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, -90f, 0), 0.1f);
+        //}
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position = LerpMoveTo(_startPosition, _endPosition, TimeToReachNextTile);
+    }
+    /// <summary>
+    /// Плавно двигаем объект
+    /// </summary>
+    /// <param name="start">начальная позиция объекта</param>
+    /// <param name="end">конечная позиция объекта</param>
+    /// <param name="time">время за которое он переместится</param>
+    /// <returns></returns>
+    private Vector3 LerpMoveTo(Vector3 start, Vector3 end, float time)
+    {
+        _time += Time.deltaTime;
+        return Vector3.Lerp(start, end, Easing.Evaluate(_time / time));
     }
 }
