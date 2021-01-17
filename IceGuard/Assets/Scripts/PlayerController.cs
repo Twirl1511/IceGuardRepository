576b37078e8dd9929ac207356b0e46be29d75a4a
Twirl1511 <64476851+Twirl1511@users.noreply.github.com>
2021-01-17 22:30:06 +0300

    первый вариант управления

--- IceGuard/Assets/Scripts/PlayerController.cs
+++ IceGuard/Assets/Scripts/PlayerController.cs
@@ -4,32 +4,105 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private Joystick joystick;
     public AnimationCurve Easing;
     [SerializeField] float TimeToReachNextTile = 3;
     private Vector3 _startPosition;
     private Vector3 _endPosition;
     private float _time;
+    [SerializeField] private int RotationSpeed = 1;
     void Start()
     {
         _startPosition = transform.position;
-        _endPosition = _startPosition + new Vector3(-1.1f, 0f, 0f);
+        //_endPosition = _startPosition + new Vector3(-1.1f, 0f, 0f);
     }
-    private void FixedUpdate()
-    {
-        Debug.Log(Input.GetAxis("Mouse X")); 
-        if(Input.GetAxis("Mouse X") > 0.5f)
-        {
-            transform.rotation = Quaternion.Euler(0f, 90f, 0f);
-        }
-        //if (Input.GetAxis("Mouse X") < -0.5f)
-        //{
-        //    transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, -90f, 0), 0.1f);
-        //}
-    }
+
 
     void Update()
     {
-        transform.position = LerpMoveTo(_startPosition, _endPosition, TimeToReachNextTile);
+        MoveForwardMouse();
+        RotateMouse();
+        //MoveForward();
+        //Rotate();
+
+    }
+    private void MoveForward()
+    {
+        if (Input.GetTouch(0).phase.ToString() == "Stationary")
+        {
+            if(_time == 0)
+            {
+                _endPosition = _startPosition + transform.forward * 1.1f;
+            }
+            
+            _time += Time.deltaTime;
+            
+            transform.position = LerpMoveTo(_startPosition, _endPosition, TimeToReachNextTile);
+        }
+        if (_time >= TimeToReachNextTile)
+        {
+            _startPosition = _endPosition;
+            _time = 0;
+        }
+    }
+    private void MoveForwardMouse()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (_time == 0)
+            {
+                _endPosition = _startPosition + transform.forward * 1.1f;
+            }
+
+            _time += Time.deltaTime;
+
+            transform.position = LerpMoveTo(_startPosition, _endPosition, TimeToReachNextTile);
+        }
+        if (_time >= TimeToReachNextTile)
+        {
+            _startPosition = _endPosition;
+            _time = 0;
+        }
+    }
+    private int y = 0;
+    private bool flag = true;
+    private void Rotate()
+    {
+        if (joystick.Horizontal < -0.8f && flag)
+        {
+            y -= 90;
+            flag = false;
+        }
+        if (joystick.Horizontal > 0.8f && flag)
+        {
+            y += 90;
+            flag = false;
+        }
+        if (_time == 0)
+        {
+            flag = true;
+        }
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, y, 0), 1f);
+    }
+    private void RotateMouse()
+    {
+        Debug.Log(Input.GetAxis("Mouse X"));
+        if (Input.GetAxis("Mouse X") < -0.8f && flag)
+        {
+            Debug.Log("left");
+            y -= 90;
+            flag = false;
+        }
+        if (Input.GetAxis("Mouse X") > 0.8f && flag)
+        {
+            y += 90;
+            flag = false;
+        }
+        if (_time == 0)
+        {
+            flag = true;
+        }
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, y, 0), 1f);
     }
     /// <summary>
     /// Плавно двигаем объект
@@ -40,7 +113,6 @@ public class PlayerController : MonoBehaviour
     /// <returns></returns>
     private Vector3 LerpMoveTo(Vector3 start, Vector3 end, float time)
     {
-        _time += Time.deltaTime;
         return Vector3.Lerp(start, end, Easing.Evaluate(_time / time));
     }
 }
