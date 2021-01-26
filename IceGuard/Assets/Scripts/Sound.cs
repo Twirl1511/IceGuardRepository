05fef440e6360d4ad265662e036d3eb5c5495bbd
Twirl1511 <64476851+Twirl1511@users.noreply.github.com>
2021-01-26 20:12:52 +0300

    в процессе звук

---
+++ IceGuard/Assets/Scripts/Sound.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound 
+{
+    public string name;
+    public AudioClip audioClip;
+    [Range(0f,1f)] public float volume;
+
+}
---
+++ IceGuard/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private Sound[] sounds;
+    public enum Soundtest
+    {
+        PlayerMove,
+        PlayerBlocked,
+        ForceFieldCreate,
+        ForceFieldDestroy,
+        ForceFieldDestroySoon,
+        MeteoriteAlert,
+        MeteoriteMove,
+        MeteoriteCrashForceField,
+        MeteoriteCrashPlayer,
+        MeteoriteCrashEarth,
+        GameOver
+    }
+   
+    public static void PlaySound()
+    {
+        
+    }
+
+
+}
