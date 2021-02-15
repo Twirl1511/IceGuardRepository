1277d476f58f1beb6e3d1478d19986f70879d4f5
Twirl1511 <64476851+Twirl1511@users.noreply.github.com>
2021-02-15 18:05:35 +0300

    прозрачность полей

--- IceGuard/Assets/Scripts/NewForceField.cs
+++ IceGuard/Assets/Scripts/NewForceField.cs
@@ -7,19 +7,45 @@ public class NewForceField : MonoBehaviour
     public static float lifeTime = 60f;
     public static float NewLifeTime;
     public float SecondsToDestroy;
-    
+    [SerializeField] private GameObject gameObjectMaterial;
+    private Material _material;
+    private float _alphaValue;
+
     void Start()
     {
+        _material = gameObjectMaterial.GetComponent<Renderer>().material;
         NewLifeTime = lifeTime;
-        SecondsToDestroy = 0;
+        SecondsToDestroy = 0.0001f;
         //StartCoroutine(LaterDestroy(SecondsToDestroy));
     }
     private void Update()
     {
         AdjustedLifeTimer();
         LifeTimer();
+        ChangeAlpha(_material, Transperency());
     }
 
+    public void ChangeAlpha(Material mat, float alphaVal)
+    {
+        Color oldColor = mat.color;
+        Color newClor = new Color(oldColor.r, oldColor.g, oldColor.b, alphaVal);
+        mat.SetColor("_Color", newClor);
+
+
+    }
+
+    public float Transperency()
+    {
+        float transperency;
+        if((transperency = (0.1f * NewLifeTime) / SecondsToDestroy) < 0.1f)
+        {
+            return 0.1f;
+        }
+        else
+        {
+            return transperency;
+        }
+    }
 
     public void LifeTimer()
     {
