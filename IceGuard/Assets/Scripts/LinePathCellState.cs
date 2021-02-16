50540cb40335fa02f4f1da989ae0a9d798bb4a1a
Twirl1511 <64476851+Twirl1511@users.noreply.github.com>
2021-02-16 19:10:16 +0300

    начал делать новый путь

---
+++ IceGuard/Assets/Scripts/LinePathCellState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinePathCellState : MonoBehaviour
+{
+
+    public bool IsPathDrown;
+
+    void Start()
+    {
+        IsPathDrown = false;
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Path"))
+        {
+            IsPathDrown = true;
+        }
+        else
+        {
+            IsPathDrown = false;
+        }
+    }
+}
---
+++ IceGuard/Assets/Scripts/TouchGlow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchGlow : MonoBehaviour
+{
+
+    public Image Pointer;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount == 1)
+        {
+            Pointer.gameObject.SetActive(true);
+            Pointer.transform.position = Input.mousePosition;
+        }
+        else
+        {
+            Pointer.gameObject.SetActive(false);
+        }
+        
+    }
+}
