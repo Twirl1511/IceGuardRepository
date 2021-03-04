01677f913778f07062b45e09b5503c77a08a60fa
Twirl1511 <64476851+Twirl1511@users.noreply.github.com>
2021-03-04 14:47:56 +0300

    поля не теряют альфу

--- IceGuard/Assets/Scripts/ForceField/NewForceField.cs
+++ IceGuard/Assets/Scripts/ForceField/NewForceField.cs
@@ -14,8 +14,8 @@ public class NewForceField : MonoBehaviour
     void Start()
     {
         StartCoroutine(LaterColliderActivate());
-        _material = gameObjectMaterial.GetComponent<Renderer>().material;
-        ChangeAlpha(_material, 0.5f);
+        //_material = gameObjectMaterial.GetComponent<Renderer>().material;
+        //ChangeAlpha(_material, 0.5f);
         NewLifeTime = lifeTime;
         SecondsToDestroy = 0.0001f;
     }
@@ -23,35 +23,35 @@ public class NewForceField : MonoBehaviour
     {
         AdjustedLifeTimer();
         LifeTimer();
-        ChangeAlpha(_material, Transperency());
+        //ChangeAlpha(_material, Transperency());
     }
 
-    public void ChangeAlpha(Material mat, float alphaVal)
-    {
-        Color oldColor = mat.color;
-        Color newClor = new Color(oldColor.r, oldColor.g, oldColor.b, alphaVal);
-        mat.SetColor("_Color", newClor);
+    //public void ChangeAlpha(Material mat, float alphaVal)
+    //{
+    //    Color oldColor = mat.color;
+    //    Color newClor = new Color(oldColor.r, oldColor.g, oldColor.b, alphaVal);
+    //    mat.SetColor("_Color", newClor);
 
 
-    }
+    //}
 
-    public float Transperency()
-    {
-        float transperency;
-        if((transperency = (0.1f * NewLifeTime) / SecondsToDestroy) < 0.5f)
-        {
-            if (transperency >= 1f)
-            {
-                return 1;
-            }
-            return 0.5f;
-        }
-        else
-        {
-            return transperency;
-        }
+    //public float Transperency()
+    //{
+    //    float transperency;
+    //    if((transperency = (0.1f * NewLifeTime) / SecondsToDestroy) < 0.5f)
+    //    {
+    //        if (transperency >= 1f)
+    //        {
+    //            return 1;
+    //        }
+    //        return 0.5f;
+    //    }
+    //    else
+    //    {
+    //        return transperency;
+    //    }
         
-    }
+    //}
 
     public void LifeTimer()
     {
